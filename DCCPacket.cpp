bd29c4d304fc2bb0035287996718f2dd1d51ad5a
Railstars <dgoodman@artificial-science.org>
2010-12-27 19:31:26 -0600

    Fixed example sketch location. Fixed 4-digit addressing, with thanks to <dpharris@telus.net>

--- DCCPacket.cpp
+++ DCCPacket.cpp
@@ -9,39 +9,29 @@ DCCPacket::DCCPacket(unsigned int decoder_address) : address(decoder_address), s
 byte DCCPacket::getBitstream(byte rawbytes[]) //returns size of array.
 {
   int total_size = 1; //minimum size
-  if(address < 127) //TODO IS THIS RIGHT?
+  if(address <= 127) //TODO IS THIS RIGHT?
   {
     rawbytes[0] = (byte)address;
-    //Serial.println(rawbytes[0],BIN);
   }
-  else //ELSE WHAT?
+  else //we have an extended ("4-digit") address
   {
-    rawbytes[0] = (byte)address >> 8;
+    rawbytes[0] = (byte)((address >> 8)|0xC0);
     rawbytes[1] = (byte)(address & 0xFF);
-    //Serial.print(rawbytes[0],BIN);
-    //Serial.print(" ");
-    //Serial.println(rawbytes[1],BIN);
     ++total_size;
   }
-  int i;
-  //Serial.print("Payload:");
+  
+  byte i;
   for(i = 0; i < size; ++i,++total_size)
   {
     rawbytes[total_size] = data[i];
-    //Serial.print(" ");
-    //Serial.print(rawbytes[total_size],BIN);
   }
-  //Serial.println("");
-  //Serial.print("XOR: ");
-  //Serial.print(total_size);
-  //Serial.print(" ");
+
   byte XOR = 0;
   for(i = 0; i < total_size; ++i)
   {
     XOR ^= rawbytes[i];
   }
   rawbytes[total_size] = XOR;
-  //Serial.println(rawbytes[total_size],BIN);
 
   return total_size+1;  
 }
